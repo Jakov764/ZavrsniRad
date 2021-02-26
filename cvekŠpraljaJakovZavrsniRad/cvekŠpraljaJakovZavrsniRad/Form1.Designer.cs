51b037c655619dcb9a68e6ff4cdb57c08ef93dc7
Jakov764 <55740028+Jakov764@users.noreply.github.com>
2021-02-26 20:17:51 +0100

    Add files via upload

---
+++ cvekŠpraljaJakovZavrsniRad/cvekŠpraljaJakovZavrsniRad/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace cvekŠpraljaJakovZavrsniRad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCoko = new System.Windows.Forms.Button();
+            this.btnVan = new System.Windows.Forms.Button();
+            this.btnJag = new System.Windows.Forms.Button();
+            this.btnKorn = new System.Windows.Forms.Button();
+            this.btnCasa = new System.Windows.Forms.Button();
+            this.btnStapic = new System.Windows.Forms.Button();
+            this.btnGotov = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnPromet = new System.Windows.Forms.Button();
+            this.btnZatvori = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRacun = new System.Windows.Forms.Button();
+            this.btnKeks = new System.Windows.Forms.Button();
+            this.btnPist = new System.Windows.Forms.Button();
+            this.btnStrac = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCoko
+            // 
+            this.btnCoko.Location = new System.Drawing.Point(129, 54);
+            this.btnCoko.Name = "btnCoko";
+            this.btnCoko.Size = new System.Drawing.Size(71, 74);
+            this.btnCoko.TabIndex = 0;
+            this.btnCoko.Text = "Coko";
+            this.btnCoko.UseVisualStyleBackColor = true;
+            // 
+            // btnVan
+            // 
+            this.btnVan.Location = new System.Drawing.Point(243, 54);
+            this.btnVan.Name = "btnVan";
+            this.btnVan.Size = new System.Drawing.Size(71, 74);
+            this.btnVan.TabIndex = 1;
+            this.btnVan.Text = "Vanilija";
+            this.btnVan.UseVisualStyleBackColor = true;
+            // 
+            // btnJag
+            // 
+            this.btnJag.Location = new System.Drawing.Point(357, 54);
+            this.btnJag.Name = "btnJag";
+            this.btnJag.Size = new System.Drawing.Size(71, 74);
+            this.btnJag.TabIndex = 2;
+            this.btnJag.Text = "Jagoda";
+            this.btnJag.UseVisualStyleBackColor = true;
+            // 
+            // btnKorn
+            // 
+            this.btnKorn.Location = new System.Drawing.Point(129, 321);
+            this.btnKorn.Name = "btnKorn";
+            this.btnKorn.Size = new System.Drawing.Size(71, 74);
+            this.btnKorn.TabIndex = 5;
+            this.btnKorn.Text = "Kornet";
+            this.btnKorn.UseVisualStyleBackColor = true;
+            // 
+            // btnCasa
+            // 
+            this.btnCasa.Location = new System.Drawing.Point(243, 321);
+            this.btnCasa.Name = "btnCasa";
+            this.btnCasa.Size = new System.Drawing.Size(71, 74);
+            this.btnCasa.TabIndex = 4;
+            this.btnCasa.Text = "Casa";
+            this.btnCasa.UseVisualStyleBackColor = true;
+            // 
+            // btnStapic
+            // 
+            this.btnStapic.Location = new System.Drawing.Point(357, 321);
+            this.btnStapic.Name = "btnStapic";
+            this.btnStapic.Size = new System.Drawing.Size(71, 74);
+            this.btnStapic.TabIndex = 3;
+            this.btnStapic.Text = "Štapić";
+            this.btnStapic.UseVisualStyleBackColor = true;
+            // 
+            // btnGotov
+            // 
+            this.btnGotov.Location = new System.Drawing.Point(492, 82);
+            this.btnGotov.Name = "btnGotov";
+            this.btnGotov.Size = new System.Drawing.Size(83, 94);
+            this.btnGotov.TabIndex = 12;
+            this.btnGotov.Text = "Gotov Sladoled";
+            this.btnGotov.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(608, 82);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(279, 313);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 99);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(102, 103);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 307);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(102, 103);
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnPromet
+            // 
+            this.btnPromet.Location = new System.Drawing.Point(453, 491);
+            this.btnPromet.Name = "btnPromet";
+            this.btnPromet.Size = new System.Drawing.Size(75, 46);
+            this.btnPromet.TabIndex = 17;
+            this.btnPromet.Text = "Promet";
+            this.btnPromet.UseVisualStyleBackColor = true;
+            // 
+            // btnZatvori
+            // 
+            this.btnZatvori.Location = new System.Drawing.Point(568, 491);
+            this.btnZatvori.Name = "btnZatvori";
+            this.btnZatvori.Size = new System.Drawing.Size(75, 46);
+            this.btnZatvori.TabIndex = 18;
+            this.btnZatvori.Text = "Zatvori";
+            this.btnZatvori.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Okus";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Oblik";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(605, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Racun";
+            // 
+            // btnRacun
+            // 
+            this.btnRacun.Location = new System.Drawing.Point(492, 209);
+            this.btnRacun.Name = "btnRacun";
+            this.btnRacun.Size = new System.Drawing.Size(83, 94);
+            this.btnRacun.TabIndex = 23;
+            this.btnRacun.Text = "Ispisi racun";
+            this.btnRacun.UseVisualStyleBackColor = true;
+            // 
+            // btnKeks
+            // 
+            this.btnKeks.Location = new System.Drawing.Point(357, 185);
+            this.btnKeks.Name = "btnKeks";
+            this.btnKeks.Size = new System.Drawing.Size(71, 74);
+            this.btnKeks.TabIndex = 26;
+            this.btnKeks.Text = "Keks";
+            this.btnKeks.UseVisualStyleBackColor = true;
+            // 
+            // btnPist
+            // 
+            this.btnPist.Location = new System.Drawing.Point(243, 185);
+            this.btnPist.Name = "btnPist";
+            this.btnPist.Size = new System.Drawing.Size(71, 74);
+            this.btnPist.TabIndex = 25;
+            this.btnPist.Text = "Pistacio";
+            this.btnPist.UseVisualStyleBackColor = true;
+            // 
+            // btnStrac
+            // 
+            this.btnStrac.Location = new System.Drawing.Point(129, 185);
+            this.btnStrac.Name = "btnStrac";
+            this.btnStrac.Size = new System.Drawing.Size(71, 74);
+            this.btnStrac.TabIndex = 24;
+            this.btnStrac.Text = "Stracatela";
+            this.btnStrac.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 565);
+            this.Controls.Add(this.btnKeks);
+            this.Controls.Add(this.btnPist);
+            this.Controls.Add(this.btnStrac);
+            this.Controls.Add(this.btnRacun);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnZatvori);
+            this.Controls.Add(this.btnPromet);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnGotov);
+            this.Controls.Add(this.btnKorn);
+            this.Controls.Add(this.btnCasa);
+            this.Controls.Add(this.btnStapic);
+            this.Controls.Add(this.btnJag);
+            this.Controls.Add(this.btnVan);
+            this.Controls.Add(this.btnCoko);
+            this.Name = "Form1";
+            this.Text = "Blagajna";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCoko;
+        private System.Windows.Forms.Button btnVan;
+        private System.Windows.Forms.Button btnJag;
+        private System.Windows.Forms.Button btnKorn;
+        private System.Windows.Forms.Button btnCasa;
+        private System.Windows.Forms.Button btnStapic;
+        private System.Windows.Forms.Button btnGotov;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnPromet;
+        private System.Windows.Forms.Button btnZatvori;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRacun;
+        private System.Windows.Forms.Button btnKeks;
+        private System.Windows.Forms.Button btnPist;
+        private System.Windows.Forms.Button btnStrac;
+    }
+}
+
